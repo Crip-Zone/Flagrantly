e85140cca249e0d3a3d35e8803966db3927e60b2
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2023-05-24 15:09:17 -0400

    Add files via upload

---
+++ languages/Settings.english.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.1.0; Settings
+
+global $settings;
+
+// argument(s): images_url as saved in settings
+$txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
+$txt['theme_description'] = 'The Flagrantly theme from Crip Zone Themes.<br><br>Author: Crip<br><br>Updated to v2.1 by The Crip Zone Team:<br>Skhilled, Dave, Lesmond, TwitchisMental<br><br>
+                     ';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,4 @@
+<?php
+$txt['copyrights'] = 'Copyrights';
+$txt['themecopyright'] = '<a href="https://www.simplemachines.org/community/index.php?action=profile;u=218416">Aurora SMF Theme Made By : TwitchisMental</a>';
+?>
