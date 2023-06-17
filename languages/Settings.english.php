9bf86f88b33495b7dca125fbc8ef023b12defb09
Crip Zone <9021853+Crip-Zone@users.noreply.github.com>
2023-06-17 15:37:16 -0400

    Reflects version.

--- languages/Settings.english.php
+++ languages/Settings.english.php
@@ -5,7 +5,6 @@ global $settings;
 
 // argument(s): images_url as saved in settings
 $txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
-$txt['theme_description'] = 'The Flagrantly theme from Crip Zone Themes.<br><br>Author: Crip<br><br>Updated to v2.1 by The Crip Zone Team:<br>Skhilled, Dave, Lesmond, TwitchisMental<br><br>
-                     ';
+$txt['theme_description'] = 'The Flagrantly theme from Crip Zone Themes.<br><br>Author: Crip<br><br>Updated to v2.1.3 by The CripZone';
 
 ?>
